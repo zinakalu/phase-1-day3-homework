737b68c9e286425a54009a4f6267dc3bb21f1d5c
Jeryel Blanco <jeryelblanco@gmail.com>
2023-02-23 18:33:46 -0500

    Homework

---
+++ index.js
@@ -0,0 +1,43 @@
+//TODO: Look at the HTML and see what id you need to access
+const catContainer = document.getElementById("cat-container")
+//TODO: Open your json server
+//! If you need to a refresher on this, the command for this is
+//! pineed to the slack channel
+//TODO: Fetch the cats that exist in the database
+//TODO: Create a div element for each cat and append to "cat-container"
+//TODO: Iterate through your data and add it to the cat div,
+//TODO: Create an h3, img, and p tag to hold your data and
+//TODO: append it to the created div
+//////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////
+//TODO: Fetch again and repeat to add dogs to the "dog-container"
+//////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////
